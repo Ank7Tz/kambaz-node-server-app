12eb122cc1f6a89d0d8d0ca7ccdbd465ba41f65f
Ankit Bando <ankitbando@gmail.com>
2025-11-14 17:42:49 -0500

    initial commit

---
+++ Hello.ts
@@ -0,0 +1,10 @@
+import type { Express } from 'express';
+
+export default function Hello(app: Express) {
+    app.get('/hello', (req, res) => {
+        res.send('Hello World!')
+    });
+    app.get('/', (req, res) => {
+        res.send('Welcome to Full Stack Development!')
+    });
+}
